0ffd6cba88a73e2da6db3ece51806192308b8cfc
AyselBayramova <13868@holbertonstudents.com>
2026-05-16 08:46:34 +0200

    Task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains State class and Base, an instance of declarative_base().
-"""
+"""Defines the State class and Base"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,10 +8,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    """
-    State class that links to the MySQL table states.
-    """
+    """Represents a state table"""
+
     __tablename__ = 'states'
 
-    id = Column(Integer, primary key=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
